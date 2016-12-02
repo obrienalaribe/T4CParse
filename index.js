c932032cda2ea9ef9b2e20410338d60c43b250e7
Obrien Alaribe <skiveservices@gmail.com>
2016-12-02 02:14:18 +0000

    server url

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ if (!riderPushCert || !driverPushCert) {
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 
 var api = new ParseServer({
-  serverURL: mountPath,
+  serverURL: "https://transportforchurch.herokuapp.com/parse",
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'localAppId',
